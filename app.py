1e84398312820baae71b52b0b06d32a18c86e5df
Kieran Collins <kiecol@protonmail.com>
2023-04-05 16:45:19 +0000

    Update app: change questions, improve UI & UX

--- app.py
+++ app.py
@@ -16,27 +16,37 @@ def index():
 
 @app.route('/generate_itinerary', methods=['POST'])
 def generate_itinerary():
-    destination = request.form['destination']
-    budget = request.form['budget']
-    duration = request.form['duration']
-    activities = request.form['activities']
-    accommodation = request.form['accommodation']
 
-    prompt = f"Here's an itinerary for your ideal holiday to {destination}:"
-    input_text = f"Destination: {destination}\nBudget: {budget}\nDuration: {duration}\nActivities: {activities}\nAccommodation: {accommodation}\n{prompt}\n"
+    destination = request.form['destination']
+    climate = request.form['climate']
+    activities = request.form['activities']
+    group = request.form['group']
+    accommodation = request.form['accommodation']
+    relaxing = request.form['relaxing']
+    cuisine = request.form['cuisine']
+    attractions = request.form['attractions']
+    transportation = request.form['transportation']
+    cultural = request.form['cultural']
+    duration = request.form['duration']
+    hobbies = request.form['hobbies']
+    itinerary = request.form['itinerary']
+    other = request.form['other']
+
+    prompt = f"Create a detailed dream holiday itinerary using the following information:"
+    input_text = f"{prompt}\nDestination: {destination}\nClimate: {climate}\nActivities: {activities}\nGroup: {group}\nAccommodation: {accommodation}\nRelaxing/Adventurous: {relaxing}\nCuisine: {cuisine}\nAttractions: {attractions}\nTransportation: {transportation}\nCultural Experiences/Historical Sites: {cultural}\nDuration: {duration}\nHobbies/Interests: {hobbies}\nItinerary preference: {itinerary}\nOther: {other}\n"
 
     response = openai.Completion.create(
       engine="text-davinci-003",
       prompt=input_text,
       temperature=0.7,
-      max_tokens=1000,
+      max_tokens=1250,
       top_p=1,
       frequency_penalty=0,
       presence_penalty=0
     )
 
     itinerary = response.choices[0].text
-    return itinerary
+    return f"Your Custom Itinerary, courtesy of eTravel.Guru: {itinerary}"
 
 
 if __name__ == '__main__':
